1049f6819907fc2d0c324d7893039f04c068d8ba
José Alceu <106621012+josealceu16@users.noreply.github.com>
2023-03-07 10:47:30 -0300

    Create Valores.js

---
+++ Valores.js
@@ -0,0 +1,6 @@
++//  agora vamos ver como posso pegar valores em html. 
+//para isso usamos o seguinte comando = document.getElementById('nomedeumavariavel/class/Id').innerHTML;
+document.getElementById('').innerHTML;
+
+
+//                      Js é uma linguagem de tipagem fraca, ou seja, podemos atribuir varias variaveis em um só.
