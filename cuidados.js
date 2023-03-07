19f7d4b223e6819f3ad18041ab1945c64545387c
José Alceu <106621012+josealceu16@users.noreply.github.com>
2023-03-07 10:49:12 -0300

    Create cuidados.js

---
+++ cuidados.js
@@ -0,0 +1,4 @@
+//quando mais zeros eu colocar antes do 1 maior vai ser meu resultado, e se colocar zero vai dar um valor infinity;
+console.log(7 / 0.0001);
+//como js é uma linguagem fraca simplesmente eu posso dividir uma string, veja:
+console.log("10" / 2)
