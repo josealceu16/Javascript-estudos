1b0526a4ba8815a2e61199a9716334013b30d5ec
José Alceu <106621012+josealceu16@users.noreply.github.com>
2023-03-07 10:47:05 -0300

    Create Var-let-Const.js

---
+++ Var-let-Const.js
@@ -0,0 +1,33 @@
+// o básico de var - let e const //
+
+/*como declaramos uma variável em Js. Ex. temos variável (var ou let)
+depois temos o valor que vai receber aquela variavel (a= ex) e depois colocamos o que vai receber.
+(=valor)
+*/
+//agora vamos criar o nosso console.log para mostrar o valor; e dentro vai ficar o valor da nossa variavel; 
+var a = 3;
+console.log(a);
+
+
+//temos tambem os valores com (let)
+let b = 2;
+console.log(b);
+//variavel (const)
+const c = 1;
+console.log(c);
+
+//DE PREFERENCIA UTILIZE SEMPRE A VARIAVEL //LET// EM VEZ DE //VAR//
+
+/* por mais que eu utilizeis todas essas variaveis e por mais que pareçam quem tenham a mesma função 
+elas são diferentes*/
+
+
+
+//podemoos tambem pegar minha variavel e descobrir qual é o seu tipo. 
+
+console.log(typeof c);
+//logo minha minha variavel recebe um valor do tipo (number)
+
+let nome = 'Olá, Mundo!';
+console.log(typeof nome);
+//o valor que minha vaariável nome acabou de reber é do tipo string.
