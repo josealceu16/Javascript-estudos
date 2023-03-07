e537ea4b99a72c4bc8a04f4e60c0aa4a6a9bd800
José Alceu <106621012+josealceu16@users.noreply.github.com>
2023-03-07 10:48:12 -0300

    Create tipos.js

---
+++ tipos.js
@@ -0,0 +1,17 @@
+// modo de como eu posso ver se meu valor é do tipo inteiro ou não.
+const teste1 = 2.0;
+console.log('teste1');
+console.log(Number.isInteger(teste1))
+
+//um exemplo de operação aritimética; 
+
+const number = 4.0234;
+const peso1 = 54.8;
+const peso2 = 80.14;
+const number2 = 5000
+const resultado = number * peso1 + peso2 * number2;
+console.log(number, peso1, peso2, number2);
+console.log(resultado.toFixed(2));
+//colocando o atributo .toFixed(totaldecasasquequeropular)); com isso posso tirar o numero de casas que ficam depois da casa decimal.
+
+
