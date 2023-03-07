8b1fc1e85952ac83499dd139e200038eada06aad
José Alceu <106621012+josealceu16@users.noreply.github.com>
2023-03-07 10:48:47 -0300

    Create Organização do código.js

---
+++ Organização do código.js
@@ -0,0 +1,12 @@
+//criando minha primeira mensagem um javascript
+console.log("Olá, Mundo!")
+//em jjavascript tambem podemos tambem ter um codigo em Js dentro de um bloco; 
+{
+    console.log("isso está dentro de um bloco.")
+}
+//E agora temos um bloco dentro de outro bloco.
+{
+    {
+        console.log("Outro ex. só que é um bloco dentro de outro.")
+    }
+}
